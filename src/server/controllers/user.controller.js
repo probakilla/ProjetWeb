4021a1992331a9bf80232e8fb480ef182e4dd854
probakilla <probakilla@gmail.com>
2019-01-04 16:02:11 +0100

    implémentation de la requête d'authentification

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -1,21 +1,39 @@
 const User = require("../models/user.model");
-const SHA256 = require("crypto-js/sha256") 
+const CryptoJS = require("crypto-js");
+const key = "CryptoKey";
+
+function handleError(err, res) {
+  res.status(400).send("DATABASE ERROR: " + err);
+}
+
+function ecr(word) {
+  return CryptoJS.AES.encrypt(word, key);
+}
+
+function dcr(encrypted) {
+  return CryptoJS.AES.decrypt(encrypted, key).toString(CryptoJS.enc.Utf8);
+}
 
 exports.userRegister = (req, res) => {
-    let password = SHA256(req.body.password);
-    let user = new User(
-        {
-            username: req.body.username,
-            password: password,
-            labs: req.body.labs,
-            teams: req.body.teams
-        }
-    );
+  const password = ecr(req.body.password);
+  let user = new User({
+    username: req.body.username,
+    password: password,
+    labs: req.body.labs,
+    teams: req.body.teams
+  });
+  user.save(err => {
+    if (err) handleError(err, res);
+    res.status(201).send("Successfully registred!");
+  });
+};
 
-    user.save(err => {
-        if (err) {
-            alert("Error!!");
-        }
-        res.send("Successfully registred!");
-    })
-}
+exports.userCredentials = (req, res) => {
+  User.findOne({ username: req.params.uname }, "password", (err, user) => {
+    if (err) handleError(err, res);
+    if (dcr(user.password) === req.params.uname) {
+      res.status(202).send("Connected");
+    }
+    res.status(401).send("Authentication failed");
+  });
+};
--- src/server/routes/user.route.js
+++ src/server/routes/user.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.post("/", userController.userRegister);
+router.get("/:uname&:pswd", userController.userCredentials);
 
 module.exports = router;
