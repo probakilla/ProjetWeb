88d1edafb205e41a501241e1fee1c1f8a2b2a0e2
probakilla <probakilla@gmail.com>
2019-01-04 13:05:18 +0100

    Encryptage du mot de passe lors de l'enregistrement

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -1,14 +1,14 @@
 const User = require("../models/user.model");
-
-exports.test = (req, res) => {
-    res.send("User test controller");
-};
+const SHA256 = require("crypto-js/sha256") 
 
 exports.userRegister = (req, res) => {
+
+
+    let password = SHA256(req.body.password);
     let user = new User(
         {
             username: req.body.username,
-            password: req.body.password,
+            password: password,
             labs: req.body.labs,
             teams: req.body.teams
         }
