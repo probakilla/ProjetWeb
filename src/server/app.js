9018ff9a5a7b978a68b0a16bfe367cdd977a0a21
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-10 16:43:47 +0100

    Ajout d'une route / sur l'api

--- src/server/app.js
+++ src/server/app.js
@@ -3,16 +3,17 @@ const user = require("./routes/user.route");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Console = require("console");
 
 const dbUrl = "mongodb://admin:yoloswagg69@ds026658.mlab.com:26658/projetweb";
 const mongo = process.env.MONGODB_URI || dbUrl;
 mongoose.connect(mongo);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("error", Console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
-const port = 4444;
+const port = process.env.PORT || 4444;
 
 app.use(express.static("dist"));
 app.use(cors());
@@ -20,4 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/user", user);
 
-app.listen(process.env.PORT || port);
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the internal API!");
+})
+
+app.listen(port, () => {
+  Console.log("Listening on port " + port);
+});
