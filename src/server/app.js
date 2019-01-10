ed669e2cb9ef3b9e3096d7511d302878ddcd1de0
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-10 18:35:50 +0100

    suppression d'un deprecation warning

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,7 @@ const http = require("http");
 
 const dbUrl = "mongodb://admin:yoloswagg69@ds026658.mlab.com:26658/projetweb";
 const mongo = process.env.MONGODB_URI || dbUrl;
-mongoose.connect(mongo);
+mongoose.connect(mongo, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error", Console.error.bind(console, "MongoDB connection error:"));
