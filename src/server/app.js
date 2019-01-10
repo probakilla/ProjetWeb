0eeb8283b40a93c0b27e7a5f3744ebd700d23a7b
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-10 17:51:19 +0100

    Ajout d'un module npm pour le serveur

--- src/server/app.js
+++ src/server/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const Console = require("console");
 const path = require("path");
+const http = require("http");
 
 const dbUrl = "mongodb://admin:yoloswagg69@ds026658.mlab.com:26658/projetweb";
 const mongo = process.env.MONGODB_URI || dbUrl;
@@ -14,18 +15,16 @@ const db = mongoose.connection;
 db.on("error", Console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
+const server = http.Server(app);
 const port = process.env.PORT || 4444;
 
 app.use(express.static("dist"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "dist")))
 app.use("/user", user);
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.html"));
-})
-
-app.listen(port, () => {
-  Console.log("Listening on port " + port);
+server.listen(port, () => {
+  Console.log(`Listening on port http://localhost:${port}/`);
 });
